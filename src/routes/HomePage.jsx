13e2aa23d193474316e900711e6e09ffb7b1ae1c
Your Name <you@example.com>
2024-09-04 13:46:54 +0200

    merg HomePage

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,15 +1,12 @@
 import React, { useContext } from "react";
 import { ProductProvider, ProductContext } from "/src/context/ProductProvider.jsx";
-import React from "react";
-import Store from "../Store"
 
 function HomePage() {
 	const { products, setProducts, singleProduct, setSingleProduct } = useContext(ProductContext);
 
 	return (
-		<div className="flex items-center justify-center  bg-gray-100">
-		
-			<Store/>
+		<div className="flex items-center justify-center h-screen bg-gray-100">
+			<h2 className="text-4xl font-bold text-blue-500">Home Page</h2>
 		</div>
 	);
 }
