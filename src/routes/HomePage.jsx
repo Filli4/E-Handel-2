e320aa0a12dadddaeab01b79ec7704c520782fbc
husseakan <huseyinakan2002@gmail.com>
2024-09-04 13:33:44 +0200

    Product Card is done

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,11 +1,10 @@
 import React from "react";
+import Card from "../components/Card";
 
 function HomePage() {
-	return (
-		<div className="flex items-center justify-center h-screen bg-gray-100">
-			<h2 className="text-4xl font-bold text-blue-500">Home Page</h2>
-		</div>
-	);
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100"></div>
+  );
 }
 
 export default HomePage;
