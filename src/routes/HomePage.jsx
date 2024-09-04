46e1875aef5097b23a5c7eb2bc7f66d8f0944fed
ozayo <ozay.ozdemir@gmail.com>
2024-09-04 10:44:01 +0200

    add product page

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function HomePage() {
 	return (
 		<div className="flex items-center justify-center h-screen bg-gray-100">
-			<h2 className="text-4xl font-bold text-blue-500">Home Page Ozay</h2>
+			<h2 className="text-4xl font-bold text-blue-500">Home Page</h2>
 		</div>
 	);
 }
--- src/routes/ProductPage.jsx
+++ src/routes/ProductPage.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 function ProductPage() {
-	return <h2>Product Page</h2>;
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/${id}`)
+      .then(response => response.json())
+      .then(data => setProduct(data))
+      .catch(err => console.error("Error fetching data:", err));
+  }, [id]);
+
+  if (!product) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+			<h1>{product.title}</h1>
+			<p>{product.rating.rate }</p>
+			<img src={product.image} alt={product.title} style={{ width: '200px' }} />
+			<p>Price: ${product.price}</p>
+			<p>Category: {product.category}</p>
+      <p>{product.description}</p>
+      
+    </div>
+  );
 }
 
 export default ProductPage;
