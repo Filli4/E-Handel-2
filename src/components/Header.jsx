8721d5c8c0b865f765ff4f79ab4eb8768644e759
ozayo <ozay.ozdemir@gmail.com>
2024-09-05 10:45:46 +0200

    added link on logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,10 @@ import { ProductProvider, ProductContext } from "/src/context/ProductProvider.js
 const Header = () => {
 	const { products, setProducts, singleProduct, setSingleProduct } = useContext(ProductContext);
 	return (
-		<header className="flex flex-row items-center justify-center w-full h-20 bg-gray-100">
-			<div className="flex flex-row w-2/3 h-full px-10 items-center justify-between bg-gray-300 rounded-b-md">
+		<header>
+			<div className="flex flex-row w-full h-full px-10 py-5 items-center justify-between bg-gray-300 rounded-b-md">
 				<p className="flex text-xl font-semibold tracking-wider cursor-default select-none gap-1">
-					OZELL<i className="fa-solid fa-comment-dollar text-md"></i>
+					<Link to={"/"}>OZELL<i className="fa-solid fa-comment-dollar text-md"></i></Link>
 				</p>
 				<nav className="flex gap-8 text-sm font-medium tracking-wide">
 					<Link to={"/"}>HOME</Link>
