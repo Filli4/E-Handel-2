2b8e32b106e812c1a131343659ecc9e93c3e1576
why_code <119089018+VWiman@users.noreply.github.com>
2024-09-05 11:36:36 +0200

    Fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./routes/HomePage";
 import ProductPage from "./routes/ProductPage";
 import CartPage from "./routes/CartPage";
+import Shop from "./routes/Shop";
 
 function App() {
-  return (
+	return (
 		<main>
 			<Routes>
 				<Route path="/" element={<HomePage />} />
-
-				<Route path="/cartpage" element={<CartPage />} />
+				<Route path="/shop" element={<Shop />} />
+				<Route path="/cart" element={<CartPage />} />
 				{/* <Route path="/productPage" element={<ProductPage />} /> <-- use for no product found */}
 				<Route path="/product/:id" element={<ProductPage />} />
 			</Routes>
