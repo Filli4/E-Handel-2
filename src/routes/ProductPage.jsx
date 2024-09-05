9ec8f01a88ea435fc8ccf58804df31f3f096dc87
ozayo <ozay.ozdemir@gmail.com>
2024-09-05 10:47:03 +0200

    added style

--- src/routes/ProductPage.jsx
+++ src/routes/ProductPage.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { ProductProvider, ProductContext } from "/src/context/ProductProvider.jsx"; 
 import FetchSingleItem from "../services/FetchSingleItem";
+import AddToCart from "../components/Buttons/AddToCart.jsx";
 
 function ProductPage() {
 	const { products, setProducts, singleProduct, setSingleProduct } = useContext(ProductContext);
@@ -20,13 +21,26 @@ function ProductPage() {
 	}
 
 	return (
-		<div>
-			<h1>{singleProduct.title}</h1>
-			<p>{singleProduct.rating.rate}</p>
-			<img src={singleProduct.image} alt={singleProduct.title} style={{ width: "200px" }} />
-			<p>Price: ${singleProduct.price}</p>
-			<p>Category: {singleProduct.category}</p>
-			<p>{singleProduct.description}</p>
+		<div className="flex flex-row py-8 gap-3">
+			<div className="w-1/2 p-4">
+				<div className="max-w-96">
+					<img className="object-cover " src={singleProduct.image} alt={singleProduct.title} />
+				</div>
+			</div>
+			<div className="w-1/2 flex flex-col justify-between gap-3">
+				<div className="flex flex-col gap-5">
+					<h1>{singleProduct.title}</h1>
+					<p className="pricetag">{singleProduct.price} <span>sek</span></p>
+					<div className="flex flex-col justify-between">
+						<p>Rating: {singleProduct.rating.rate}</p>
+						<p>Category: {singleProduct.category}</p>
+					</div>
+					
+					<h3>Description</h3>
+					<p>{singleProduct.description}</p>
+				</div>
+				<AddToCart product={singleProduct} />
+			</div>
 		</div>
 	);
 }
