aaca759bdff997f8d1c633ad60ca363f224dd124
ozayo <ozay.ozdemir@gmail.com>
2024-09-06 14:11:08 +0200

    badge function added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { ProductProvider, ProductContext } from "/src/context/ProductProvider.jsx";
+import { CartContext } from '../context/CartProvider';
+
 
 const Header = () => {
 	const { products, setProducts, singleProduct, setSingleProduct } = useContext(ProductContext);
+	
+	// calculate and show total cart item in badge
+	const { cartItems } = useContext(CartContext);
+	const totalItems = cartItems.reduce((sum, item) => sum + item.quantity, 0);
+
 	return (
 		<header>
 			<div className="flex flex-row w-full h-full px-10 py-5 items-center justify-between bg-gray-300 rounded-b-md">
@@ -13,7 +20,16 @@ const Header = () => {
 				<nav className="flex gap-8 text-sm font-medium tracking-wide">
 					<Link to={"/"}>HOME</Link>
 					<Link to={"/shop"}>SHOP</Link>
-					<Link to={"/cart"}>CART</Link>
+					<div className="relative mr-2">
+						<Link to={"/cart"}>CART
+						{/* Badge: show if product have in cart */}
+								{totalItems > 0 && (
+									<span className="absolute top-0 right-25 bg-red-500 text-white rounded-full text-xs px-2 py-1">
+										{totalItems}
+									</span>
+							)}
+							</Link>
+					</div>
 					<Link to={"/cart"} className="fa fa-solid fa-user self-center"></Link>
 				</nav>
 			</div>
